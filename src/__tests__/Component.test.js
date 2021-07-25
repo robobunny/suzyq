33eb92f3ddd06dc711a863ec1ec2f27bf3bed007
William Duraney <william.duraney@gmail.com>
2021-07-24 22:15:55 -0400

    add Display tests

--- src/__tests__/Component.test.js
+++ src/__tests__/Component.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import userEvent from "@testing-library/user-event";
-import { Button } from '../components/Component.js';
+import { Button, Display } from '../components/Component.js';
 
 
 describe('Button', () => {
@@ -26,3 +26,22 @@ describe('Button', () => {
     })
 })
 
+describe('Display', () => {
+    it('should display its value', () => {
+        const {getByDisplayValue} = render(<Display value={173} index={0} onChangeInput={()=>{}}/>);
+        const disp =getByDisplayValue('173');
+        expect(disp).toBeInTheDocument();
+    })
+    it('should allow updating value directly', () => {
+        let value;
+        const index = 0;
+        const mockOnChange = jest.fn((i, v)=>{value = v});
+        const {getByPlaceholderText} = 
+            render(<Display value={value} index={index} onChangeInput={mockOnChange}/>);
+        const disp = getByPlaceholderText('Operand 1');
+        userEvent.dblClick(disp);
+        userEvent.keyboard('371');
+        expect(disp).toHaveValue('371');
+        expect(mockOnChange).toHaveBeenLastCalledWith(index, '371');
+    })
+})
