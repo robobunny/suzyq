c998a8e1e43194fc06fe2c0cdd21cb50b7273f55
William Duraney <william.duraney@gmail.com>
2021-07-24 22:16:15 -0400

    add Display component

--- src/components/Component.js
+++ src/components/Component.js
@@ -1,5 +1,12 @@
 import React from 'react';
 
-export const Button = ({value, text, onClick}) =>
-    <button onClick={()=>onClick(value)}>{text ? text : value}</button>;
+export const Button = ({value, text, icon, onClick}) =>
+    <button onClick={()=>onClick(value)}>
+        {icon}
+        {text ? text : value}
+    </button>;
+
+export const Display = ({value, index, onChangeInput}) =>
+    <input type='text' placeholder={`Operand ${index+1}`}
+        value={value} onChange={(e)=>onChangeInput(index, e.target.value)}/>;
 
